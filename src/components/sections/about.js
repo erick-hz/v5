b253ebb426c85064b7eb242da98e901c769e10ed
Erick Hernandez <yerickk8@gmail.com>
2024-06-28 23:15:50 -0600

    text update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -146,18 +146,16 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              <br />I am passionate about creating impactful and engaging online experiences. My
-              journey into web development began in 2018 when I experimented with editing custom
-              Tumblr themes. Crafting a custom reblog button introduced me to the world of web
-              development, sparking my interest in the field.
+              <br />I enjoy creating things that live on the internet. My interest in web
+              development started back in 2018 when I decided to try editing custom Tumblr themes —
+              turns out hacking together a custom reblog button taught me a lot about React JS.
             </p>
 
             <p>
-              Since then, I have honed my skills and am now a developer at{' '}
-              <a href="https://www.hellopixelbot.com/">a start-up</a>. My current focus is on
-              building accessible and inclusive products and digital experiences for a diverse range
-              of clients. I am dedicated to ensuring that the web is a welcoming and usable space
-              for everyone.
+              Fast-forward to today, and I've had the privilege of working as a developer at{' '}
+              <a href="https://www.hellopixelbot.com/">a start-up</a>. My main focus these days is
+              building accessible, inclusive products and digital experiences for a variety of
+              clients.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
